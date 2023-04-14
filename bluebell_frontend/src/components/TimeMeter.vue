fa3a334de7bb9f3e7134abc73b1283ad474929c5
china-521 <3052236335@qq.com>
2023-04-14 17:04:42 +0800

    feat：新增计时器组件

---
+++ bluebell_frontend/src/components/TimeMeter.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<el-card>
+			<div slot="header" class="clearfix">
+				<span class="web-status">🚀 网站状况</span>
+			</div>
+			<span class="run-time"></span>
+		</el-card>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TimeMeter',
+	data() {
+		return {
+			runTime: new Date("2023-4-14")
+		}
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			let free = new this.Free();
+			// 设置网站运行时间
+			free.dateUtils.runTime({
+				el: '.run-time',
+				timeStamp: this.runTime.getTime(),
+				desc: '运行时间：'
+			});
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+.web-status {
+	font-weight: 600;
+}
+
+.run-time {
+	background-image: linear-gradient(to right, orange, purple);
+	-webkit-background-clip: text;
+	color: transparent;
+}
+</style>
