8c1876d27f8ff4ae86d044743aa61e300f29d33c
huchao <2557523039@qq.com>
2023-05-07 01:10:04 +0800

    fix

--- bluebell_backend/pkg/jwt/jwt.go
+++ bluebell_backend/pkg/jwt/jwt.go
@@ -24,18 +24,19 @@ func keyFunc(_ *jwt.Token) (i interface{}, err error) {
 	return mySecret, nil
 }
 
-// TokenExpireDuration 定义JWT的过期时间
-const TokenExpireDuration = time.Hour * 24 * 7
+// AccessTokenExpireDuration 定义JWT的过期时间
+const AccessTokenExpireDuration = time.Hour * 24      // access_token 过期时间
+const RefreshTokenExpireDuration = time.Hour * 24 * 7 // refresh_token 过期时间
 
-// GenToken 生成JWT 生成access token 和 refresh token
+// GenToken 生成JWT 生成 access_token 和 refresh_token
 func GenToken(userID uint64, username string) (aToken, rToken string, err error) {
 	// 创建一个我们自己的声明
 	c := MyClaims{
 		userID,   // 自定义字段
 		username, // 自定义字段
 		jwt.StandardClaims{ // JWT规定的7个官方字段
-			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间
-			Issuer:    "bluebell",                                 // 签发人
+			ExpiresAt: time.Now().Add(AccessTokenExpireDuration).Unix(), // 过期时间
+			Issuer:    "bluebell",                                       // 签发人
 		},
 	}
 	// 加密并获得完整的编码后的字符串token
@@ -43,32 +44,10 @@ func GenToken(userID uint64, username string) (aToken, rToken string, err error)
 
 	// refresh token 不需要存任何自定义数据
 	rToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Second * 30).Unix(), // 过期时间
-		Issuer:    "bluebell",                              // 签发人
+		ExpiresAt: time.Now().Add(RefreshTokenExpireDuration).Unix(), // 过期时间
+		Issuer:    "bluebell",                                        // 签发人
 	}).SignedString(mySecret)
 	// 使用指定的secret签名并获得完整的编码后的字符串token
-	return
-}
-
-//GenToken2 生成 Token
-func GenToken2(userID uint64, username string) (Token string, err error) {
-	// 创建一个我们自己的声明
-	c := MyClaims{
-		userID,     // 自定义字段
-		"username", // 自定义字段
-		jwt.StandardClaims{ // JWT规定的7个官方字段
-			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间
-			Issuer:    "bluebell",                                 // 签发人
-		},
-	}
-	// 加密并获得完整的编码后的字符串token
-	Token, err = jwt.NewWithClaims(jwt.SigningMethodHS256, c).SignedString(mySecret)
-
-	// refresh token 不需要存任何自定义数据
-	//rToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-	//	ExpiresAt: time.Now().Add(time.Second * 30).Unix(), // 过期时间
-	//	Issuer:    "bluebell",                              // 签发人
-	//}).SignedString(mySecret)	// 使用指定的secret签名并获得完整的编码后的字符串token
 	return
 }
 
